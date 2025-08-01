ad20fb89e54fd04ffcda7e255c2056a21e6ae312
unknown <hazem.salem.work@gmail.com>
2025-08-01 20:25:58 +0300

    installed requests and added insert_products view

--- src/products/models.py
+++ src/products/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True, blank=True)
     stock = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products/')
     is_featured = models.BooleanField(default=False)
