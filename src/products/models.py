1ba7829aff22d15ff35e24dffa91e9d7ce88d88e
amira <amirakhamis885@gmail.com>
2025-08-01 19:52:55 +0300

    create models

--- src/products/models.py
+++ src/products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -13,6 +13,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products/')
     is_featured = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
