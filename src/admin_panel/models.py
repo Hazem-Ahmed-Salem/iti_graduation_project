9000293ff9b2491d64e27b2a2d4b217001d7fc8f
Hadeer Mohamed Eldesokeyy <hadeer.mohamed.eldesokey@gmail.com>
2025-08-02 08:20:42 +0300

    admin_panel with products and customers pages v1

--- src/admin_panel/models.py
+++ src/admin_panel/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    join_date = models.DateField(default=timezone.now)
+    status = models.IntegerField(choices=[(1, 'Active'), (0, 'Inactive')], default=1)
+
+    def __str__(self):
+        return self.name
