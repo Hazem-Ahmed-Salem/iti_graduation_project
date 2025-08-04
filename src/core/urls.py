69b3bc8751e94025a3b168b35e557c9773ab9e9d
m lapan <basionymohammed981@gmail.com>
2025-08-04 22:20:22 +0300

    Add templates and update cart/orders logic

--- src/core/urls.py
+++ src/core/urls.py
@@ -21,9 +21,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin_panel/', include('admin_panel.urls')),
+    path('admin_panel/', include("admin_panel.urls")),
     path('user/',include('user.urls')),
     path('', include('products.urls')),
+    path('cart/',include('cart.urls')),
+    path('orders/',include('orders.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
