dff5b6409d2235d16418d4216b27273e3b46d986
Hazem Ahmed <hazem.salem.work@gmail.com>
2025-08-06 06:31:34 +0300

    finished the profile view and next register view modified the html of the base and navbar fixed media files in the settings

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import register_view, login_view,logout_view
+from . import views
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+    path('next_register/', views.next_register_view, name='next_register'),
+    path('role_change/', views.request_role_change, name='request_role_change'),
+    path('add_address/', views.add_address_view, name='add_address'),
+    path('edit_address/<int:address_id>/', views.edit_address_view, name='edit_address'),
+    path('delete_address/<int:address_id>/', views.delete_address, name='delete_address'),
 ]
+
 
