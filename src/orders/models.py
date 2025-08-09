ddebbc113b63816dbb15695512e30882b13a1d08
Hazem Ahmed <hazem.salem.work@gmail.com>
2025-08-09 13:20:18 +0300

    reverted the database schema

--- src/orders/models.py
+++ src/orders/models.py
@@ -22,12 +22,12 @@ class Order(models.Model):
     ]
     buyer = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.JSONField(default=list)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2,default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=255,choices=Status_choices,default='confirmed')
     delivery_address = models.ForeignKey(Address, on_delete=models.CASCADE,null=True,default=None)
     payment_method = models.CharField(max_length=255,choices=payment_options,default='cash_on_delivery')
+    total_price = models.DecimalField(max_digits=11, decimal_places=2,default=0)
 
     def __str__(self):
         return f"{self.buyer}"
